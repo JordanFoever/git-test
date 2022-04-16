1288fc365ac3de080451bf42dcf9782dc38b7b07
卫江权 <1227105586@qq.com>
2022-04-16 11:25:52 +0800

    first commit

---
+++ src/main/java/com/hxut/GitTest.java
@@ -0,0 +1,12 @@
+package com.hxut;
+
+/**
+ * @author weijiangquan
+ * @date 2022/4/16 -11:13
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
+
