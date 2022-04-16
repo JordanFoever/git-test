5b1260e4709431e91a23a95022da1eb6b44e4239
卫江权 <1227105586@qq.com>
2022-04-16 11:31:45 +0800

    second commit

--- src/main/java/com/hxut/GitTest.java
+++ src/main/java/com/hxut/GitTest.java
@@ -7,6 +7,7 @@ package com.hxut;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2");
     }
 }
 
